42f6ee224378790adfc7023102960959148be654
JinHwan Kim <jxk8970@gmail.com>
2019-12-05 16:09:22 +0900

    Add introspection flag to true to enable to run for production server.

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ app.use(cors());
 //const userLoader = new DataLoader(keys => batchUsers(keys, models));
 
 const server = new ApolloServer({
+  introspection: true,
   typeDefs: schema,
   resolvers,
   formatError: error => {
