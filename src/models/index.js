479dba3b5a0dd599a026484a24eb41a07adcecaf
JinHwan Kim <jxk8970@gmail.com>
2019-12-05 14:10:50 +0900

    Add new environment variable for database URL which would be merge with Heroku's environment variable.

--- src/models/index.js
+++ src/models/index.js
@@ -5,14 +5,21 @@ import Sequelize from 'sequelize';
  * 
  */
 
-const sequelize = new Sequelize(
-  process.env.TEST_DATABASE || process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-  },
-);
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.TEST_DATABASE || process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+  );
+}
 
 /*
  * In the same file, you can physically associate all your models with 
